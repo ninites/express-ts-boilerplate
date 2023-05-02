54447fc3e09c8672775f49adc032f3a1f6ba4db9
Romain <rmnheo@gmail.com>
2023-05-02 22:59:52 +0200

    Add tests

--- server/tests/api/testApi.spec.ts
+++ server/tests/api/testApi.spec.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { app } from '../src';
+import { app } from '../../src';
 
 describe('Test the test path', () => {
   test('It should response the GET method', async () => {
